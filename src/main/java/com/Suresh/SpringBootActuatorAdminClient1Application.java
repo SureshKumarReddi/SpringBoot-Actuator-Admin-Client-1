6fcb7609735cbaba6b995a2c9a5b9135514a00c4
SureshKumarReddy <sureshkumarreddyraina@gmail.com>
2022-07-30 16:49:56 +0530

    first commit

---
+++ src/main/java/com/Suresh/SpringBootActuatorAdminClient1Application.java
@@ -0,0 +1,13 @@
+package com.Suresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorAdminClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorAdminClient1Application.class, args);
+	}
+
+}
